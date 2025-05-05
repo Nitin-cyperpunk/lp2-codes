5305099c33065c4280285c3c9ca060850f163b23
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-05-05 09:25:33 +0530

    first commit

---
+++ ANDXOR.java
@@ -0,0 +1,15 @@
+public class BitwiseString { 
+    public static void main(String[] args) { 
+        String str = "Hello World";  // Fixed string "Hello World" 
+ 
+        System.out.println("\nCharacter | ASCII  | AND with 127 | XOR with 127"); 
+        System.out.println("-------------------------------------------------"); 
+        for (int i = 0; i < str.length(); i++) { 
+            char c = str.charAt(i); 
+            int ascii = (int) c; 
+            int andResult = c & 127; 
+            int xorResult = c ^ 127; 
+            System.out.printf("    %2c   |  %5d |      %5d   |      %d\n", c, ascii, andResult, xorResult); 
+        } 
+    } 
+}
